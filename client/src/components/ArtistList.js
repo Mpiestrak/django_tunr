b85fc9279d843beee5ae70843f7e497fd2645cdf
Matt Piestrak <mattpiestrak@Matts-MacBook-Pro.local>
2018-12-06 16:15:40 -0500

    MVP achieved

---
+++ client/src/components/ArtistList.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import axios from 'axios'
+import Link from "react-router-dom/es/Link";
+
+
+class ArtistList extends Component {
+    state = {
+        artists: []
+    }
+
+    componentDidMount() {
+        axios.get('/api/artists')
+            .then((res)=>{
+                this.setState({artists: res.data})
+                console.log(res.data)
+            })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>All Artists</h1>
+                {this.state.artists.map(artist => (
+                    <Link to={`/artist/${artist.id}`}>
+                        {artist.name}
+                    </Link>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default ArtistList;
